cd515f80bacbd9ccd9d69da2f23edd2dd0abf2ef
bkcheung <b.cheung@hotmail.com>
2024-03-28 13:49:30 -0400

    feat: add hit() function to increment number of times ship has been hit

--- src/ship.ts
+++ src/ship.ts
@@ -3,5 +3,13 @@ function ship(length:number){
         length,
         hitCount:0,
         sunk:false,
+        hit(){
+            return this.hitCount++;
+        },
     }
 }
+
+const sheep = ship(3);
+sheep.hit();
+sheep.hit();
+console.log(sheep.hitCount);
