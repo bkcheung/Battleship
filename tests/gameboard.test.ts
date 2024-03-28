96375fc684c88ecc4b155159f31982315243cd57
bkcheung <b.cheung@hotmail.com>
2024-03-28 16:51:47 -0400

    test: add vertical and horizontal placeShip() tests

--- tests/gameboard.test.ts
+++ tests/gameboard.test.ts
@@ -2,10 +2,18 @@ import { GameBoard } from "../src/gameboard";
 
 const gb = GameBoard(10);
 
-it('ship placed', () => {
-    expect(gb.placeShip(3,[1,1])).toBe(true);
+it('horizontal: ship placed', () => {
+    expect(gb.placeShip(3,[1,1],'h')).toBe(true);
 });
 
-it('invalid position', () => {
-    expect(gb.placeShip(3,[10,1])).toBe(false);
-})
+it('horizontal: invalid position', () => {
+    expect(gb.placeShip(3,[10,1],'h')).toBe(false);
+});
+
+it('vertical: ship placed', () => {
+    expect(gb.placeShip(3,[1,1],'v')).toBe(true);
+});
+
+it('vertical: invalid position', () => {
+    expect(gb.placeShip(3,[1,10],'v')).toBe(false);
+});
