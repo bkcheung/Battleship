c5f9e889cd9243b329fdbbf1a5d45d99214669c1
bkcheung <b.cheung@hotmail.com>
2024-04-02 16:02:22 -0400

    feat: add game board testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { Ship } from './ship';
+import { CreateShip } from './ship';
+import { GameBoard } from './gameboard';
 
-const sheep = Ship(3);
+const sheep = CreateShip(3);
 sheep.hit();
 sheep.hit();
 sheep.hit();
 console.log(sheep.isSunk());
+
+const gb = GameBoard(10);
+gb.placeShip(3,[3,1],'v');
+console.log(gb.ships);
