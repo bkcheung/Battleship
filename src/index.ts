5044e4ab91943c2559f37da10195d3bcd6c25fa7
bkcheung <b.cheung@hotmail.com>
2024-04-11 09:45:50 -0400

    feat: add rendering for ship placement

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import "./style.css";
-import {initBoard} from './interface'
+import {initBoard, renderShips} from './interface'
 import { CreateShip } from './ship';
 import { GameBoard } from './gameboard';
 
 initBoard(10);
 const gb = GameBoard(10);
 gb.placeShip(3,[3,1],'v');
+gb.placeShip(5, [5,5], 'v');
+gb.placeShip(4, [2,5], 'h');
+
 gb.receiveAttack([3,1]);
-console.log(gb.ships);
+
+renderShips(gb, "pBoard");
--- src/interface.ts
+++ src/interface.ts
@@ -1,8 +1,28 @@
+import { Board } from "./gameboard";
+import { Ship } from "./ship";
+
 export function initBoard(size:number){
     const cBoard = document.getElementById('cBoard');
     cBoard.appendChild(drawBoard(size));
     const pBoard = document.getElementById('pBoard');
     pBoard.appendChild(drawBoard(size));
+}
+
+export function renderShips(gameboard: Board, id:string){
+    const ships = gameboard.ships;
+    ships.forEach((ship:Ship) => {
+        const coords = ship.allCoords();
+        coords.forEach((coord:number[])=>{
+            rShipPresent(coord, id);
+        })
+    })
+}
+
+function rShipPresent(coord: number[], id:string){
+    const board = document.getElementById(id);
+    const numSq = board.getElementsByClassName('bodySq');
+    const index = coord[0]*10 + coord[1];
+    numSq[index].classList.add('shipPresent');
 }
 
 function drawBoard(size:number){
@@ -21,6 +41,7 @@ function drawBoard(size:number){
     for(let i=1; i<size+1;i++){
         const numSq = document.createElement('div');
         numSq.classList.add('numSq');
+        // numSq.classList.add(`${i}`);
         numSq.textContent = String(i);
         board.appendChild(numSq);
         for(let j=1;j<size+1;j++){
