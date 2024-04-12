b9ef7c667b345dc509137472e283e7dcb16d277b
bkcheung <b.cheung@hotmail.com>
2024-04-12 18:27:05 -0400

    ui: update html and css, adding background and graphics

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,12 @@ gb.placeShip(3,[3,1],'v');
 gb.placeShip(5, [5,5], 'v');
 gb.placeShip(4, [2,5], 'h');
 
-gb.receiveAttack([3,1]);
+const cgb = GameBoard(10);
+cgb.placeShip(3,[3,1],'h');
+cgb.placeShip(5, [8,3], 'h');
+cgb.placeShip(4, [2,8], 'v');
 
 renderShips(gb, "pBoard");
+renderShips(cgb, "cBoard");
+
+gb.receiveAttack([3,1]);
