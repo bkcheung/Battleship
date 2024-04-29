492989cea6114748fc192204257294b1b285e007
bkcheung <b.cheung@hotmail.com>
2024-04-29 14:20:13 -0400

    chore: move populateBoard() function into createPlayer function

--- src/player.ts
+++ src/player.ts
@@ -4,6 +4,7 @@ export interface Player{
     gameboard: Board,
     moves: number[][],
     genAttack: () => number[],
+    populateBoard: () => Board,
 }
 
 export function createPlayer(size:number, gameboard: Board): Player{
@@ -17,26 +18,41 @@ export function createPlayer(size:number, gameboard: Board): Player{
             };
             this.moves.push(coords);
             return coords;
+        }, 
+        populateBoard(){
+            const ships = [[5,'aircraft-carrier'],[4,'battleship'],[3,'cruiser'],[3,'submarine'],[2,'destroyer']]
+            const orients = ['h','v'];
+            let success = false;
+            for(let i=0;i<5;i++){
+                while(!success){
+                    const length = Number(ships[i][0]);
+                    const id = String(ships[i][1]);
+                    const coords = genCoords(this.gameboard.size);
+                    const orientation = orients[Math.round(Math.random())];
+                    success = this.gameboard.placeShip(id,length,coords,orientation);
+                }
+                success = false;
+            }
+            return this.gameboard;
         }
     }
 }
-
-export function populateBoard(board: Board){
-    const ships = [[5,'aircraft-carrier'],[4,'battleship'],[3,'cruiser'],[3,'submarine'],[2,'destroyer']]
-    const orients = ['h','v'];
-    let success = false;
-    for(let i=0;i<5;i++){
-        while(!success){
-            const length = Number(ships[i][0]);
-            const id = String(ships[i][1]);
-            const coords = genCoords(board.size);
-            const orientation = orients[Math.round(Math.random())];
-            success = board.placeShip(id,length,coords,orientation);
-        }
-        success = false;
-    }
-    return board;
-}
+// export function populateBoard(board: Board){
+//     const ships = [[5,'aircraft-carrier'],[4,'battleship'],[3,'cruiser'],[3,'submarine'],[2,'destroyer']]
+//     const orients = ['h','v'];
+//     let success = false;
+//     for(let i=0;i<5;i++){
+//         while(!success){
+//             const length = Number(ships[i][0]);
+//             const id = String(ships[i][1]);
+//             const coords = genCoords(board.size);
+//             const orientation = orients[Math.round(Math.random())];
+//             success = board.placeShip(id,length,coords,orientation);
+//         }
+//         success = false;
+//     }
+//     return board;
+// }
 
 function genCoords(boardSize: number){
     const randR = Math.floor(Math.random()*boardSize);
