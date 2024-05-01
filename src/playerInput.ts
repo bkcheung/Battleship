18e465e7a3fb1de7f69f680a737f091eec5ac98b
bkcheung <b.cheung@hotmail.com>
2024-05-01 16:06:26 -0400

    ui: add ship rendering during placement

--- src/playerInput.ts
+++ src/playerInput.ts
@@ -1,5 +1,5 @@
 import { Game } from "./game"; 
-import { renderShips, drawBoard } from "./interface";
+import { renderShips } from "./interface";
 
 export interface Overlay {
     game: Game,
@@ -7,7 +7,6 @@ export interface Overlay {
     ships: string[][],
     positionShip: () => void,
 }
-
 export function createOverlay(game: Game): Overlay {
     return{
         game,
@@ -17,37 +16,47 @@ export function createOverlay(game: Game): Overlay {
         positionShip(){
             let count  = 4;
             updateMsg(`Please place ${this.ships[count][1]}`);
+            updateImg(`${this.ships[count][1]}`);
             document.addEventListener('click', (event) => {
                 const sq = event.target as HTMLElement;
                 if(sq.classList.contains('placeSq')){
                     if(count>-1){
                         const length = Number(this.ships[count][0]);
                         const id = String(this.ships[count][1]);
-                        const clicked = event.target as HTMLElement;
-                        const row = Number(clicked.getAttribute('row'));
-                        const col = Number(clicked.getAttribute('col'));
-                        const coords = [row,col];
+                        const coords = getCoords(event);
                         if(this.game.player.gameboard.placeShip(id,length,coords,this.orientation)){
                             count--;
                             if(count===-1){
                                 this.game.gameDone = false;
                                 document.getElementById("playerInput").classList.add('hidden');
                                 renderShips(this.game.player);
-                            } else{
+                            } else {
                                 updateMsg(`Please place ${this.ships[count][1]}`);
+                                updateImg(this.ships[count][1]);
                             }
-                        }
-                        else updateMsg('Please select a valid position');
+                        } else updateMsg('Please select a valid position');
                     } 
                 }
             })
         }
     }
 }
-
-
-
 function updateMsg(message:string){
     const msg = document.getElementById('placementMsg');
     msg.innerHTML= message;
+}
+function getCoords(e:Event){
+    const clicked = e.target as HTMLElement;
+    const row = Number(clicked.getAttribute('row'));
+    const col = Number(clicked.getAttribute('col'));
+    return [row,col];
+}
+function updateImg(ship:string){
+    const currShip = document.getElementById('currShip');
+    const classList = currShip.classList;
+    //clear exisitng classList
+    while (classList.length > 0) {
+        classList.remove(classList.item(0));
+    }
+    currShip.classList.add(ship);
 }
