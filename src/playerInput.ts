da5efe106d25a5a4a72d265e65f0ab92f6128eee
bkcheung <b.cheung@hotmail.com>
2024-05-01 15:19:15 -0400

    refactor: update player input interface to use factory function and interface

--- src/playerInput.ts
+++ src/playerInput.ts
@@ -1,42 +1,51 @@
 import { Game } from "./game"; 
 import { renderShips, drawBoard } from "./interface";
 
-export function placeShips(game: Game){
-    positionShip(game);
-    document.getElementById('placeBoard').appendChild(drawBoard(10,'placeSq'));
+export interface Overlay {
+    game: Game,
+    orientation: string,
+    ships: string[][],
+    positionShip: () => void,
 }
 
-function positionShip(game:Game){
-    const ships = [[5,'aircraft-carrier'],[4,'battleship'],[3,'cruiser'],
-                   [3,'submarine'],[2,'destroyer']];
-    let count  = 4;
-    updateMsg(`Please place ${ships[count][1]}`);
-    document.addEventListener('click', (event) => {
-        const sq = event.target as HTMLElement;
-        if(sq.classList.contains('placeSq')){
-            if(count>-1){
-                const length = Number(ships[count][0]);
-                const id = String(ships[count][1]);
-                const orientation = 'h';
-                const clicked = event.target as HTMLElement;
-                const row = Number(clicked.getAttribute('row'));
-                const col = Number(clicked.getAttribute('col'));
-                const coords = [row,col];
-                if(game.player.gameboard.placeShip(id,length,coords,orientation)){
-                    count--;
-                    if(count===-1){
-                        game.gameDone = false;
-                        document.getElementById("playerInput").classList.add('hidden');
-                        renderShips(game.player);
-                    } else{
-                        updateMsg(`Please place ${ships[count][1]}`);
-                    }
+export function createOverlay(game: Game): Overlay {
+    return{
+        game,
+        orientation: 'h',
+        ships: [['5','aircraft-carrier'],['4','battleship'],['3','cruiser'],
+                ['3','submarine'],['2','destroyer']],
+        positionShip(){
+            let count  = 4;
+            updateMsg(`Please place ${this.ships[count][1]}`);
+            document.addEventListener('click', (event) => {
+                const sq = event.target as HTMLElement;
+                if(sq.classList.contains('placeSq')){
+                    if(count>-1){
+                        const length = Number(this.ships[count][0]);
+                        const id = String(this.ships[count][1]);
+                        const clicked = event.target as HTMLElement;
+                        const row = Number(clicked.getAttribute('row'));
+                        const col = Number(clicked.getAttribute('col'));
+                        const coords = [row,col];
+                        if(this.game.player.gameboard.placeShip(id,length,coords,this.orientation)){
+                            count--;
+                            if(count===-1){
+                                this.game.gameDone = false;
+                                document.getElementById("playerInput").classList.add('hidden');
+                                renderShips(this.game.player);
+                            } else{
+                                updateMsg(`Please place ${this.ships[count][1]}`);
+                            }
+                        }
+                        else updateMsg('Please select a valid position');
+                    } 
                 }
-                else updateMsg('Please select a valid position');
-            } 
+            })
         }
-    })
+    }
 }
+
+
 
 function updateMsg(message:string){
     const msg = document.getElementById('placementMsg');
